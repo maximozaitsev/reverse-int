e00550ea40a0e3ac796917e3035a7f0767bf4643
Maxim <104011221+maximozaitsev@users.noreply.github.com>
2023-08-07 11:34:29 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(number) {
+   
+        var reversedNumber = 0;
+        var isNegative = number < 0;
+    
+        if (isNegative) {
+            number = -number; 
+        }
+    
+        while (number > 0) {
+            var digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number = Math.floor(number / 10);
+        }
+    
+        return isNegative ? -reversedNumber : reversedNumber;
+    
+    
+};
